434cf5c82101525e757c9cb685e4ea00649068d3
MauroMontan <mauroanastacio@outlook.es>
2022-11-13 15:58:08 -0600

    template created

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Request 
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import src.images_controller as images
 app:FastAPI = FastAPI()
 
+app.mount("/src/static", StaticFiles(directory="src/static"), name="static")
+
+templates = Jinja2Templates(directory="src/templates")
+
 app.include_router(images.router)
 
+
 @app.get("/", response_class=HTMLResponse)
-def render():
-    return """
-    <form action="/images/upload" enctype="multipart/form-data" method="post">
-        <input name="file" type="file">
-        <input type="submit">
-    </form>
-    """
+def render(r:Request):
+    return templates.TemplateResponse("index.html",{"request":r})
